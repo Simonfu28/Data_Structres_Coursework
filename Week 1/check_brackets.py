948078ea23804867cbdc6b35ba586cd70b97a237
simon.fu <simon.fu@avcorp.com>
2022-05-17 13:30:23 -0700

    Completed Assignment 1

--- Week 1/check_brackets.py
+++ Week 1/check_brackets.py
@@ -1,14 +1,10 @@
 # python3
-# passes 54/54 test cases
+# passes 54/54 test cases in 0.133 seconds (cProfile)
+
 import os
-from collections import namedtuple
+import cProfile
 
-Bracket = namedtuple("Bracket", ["char", "position"])
 directory = "check_brackets tests"
-
-
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
 
 
 def find_mismatch(text):
@@ -24,6 +20,7 @@ def find_mismatch(text):
         if next in ")]}":
             if next == opening_brackets_stack[-1]:
                 opening_brackets_stack.pop(-1)
+                last_open -= 1
             else:
                 return i+1
             pass
@@ -33,26 +30,22 @@ def find_mismatch(text):
     else:
         return "Success"
 
+
 def solver(f):
     file = open(f, "r")
     return file.read()
 
 
 def main():
-
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            if f[-1] != "a":
-                file = open(f, "r")
-                test_input = file.read()
-                test_answer = solver((f + ".a"))
-                mismatch = find_mismatch(test_input)
-                if str(mismatch) == str(test_answer):
-                    print(1)
-                else:
-                    print(mismatch, test_answer, f)
+        if os.path.isfile(f) and f[-1] != "a":
+            file = open(f, "r")
+            test_input = file.read()
+            test_answer = solver((f + ".a"))
+            mismatch = find_mismatch(test_input)
+            print(mismatch, test_answer, f)
 
 
 if __name__ == "__main__":
-    main()
+    cProfile.run('main()')
