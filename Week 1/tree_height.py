bb75dea03aa44e65bbeead3843fe503e1d9e392d
Simon <simonfu28@gmail.com>
2022-06-14 15:40:11 -0700

    Completed Assignment 2

--- Week 1/tree_height.py
+++ Week 1/tree_height.py
@@ -1,29 +1,49 @@
 # python3
+# passes 24/24 tests; 0.335 sec runtime on test #24
 
 import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
 class TreeHeight:
-        def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
+        def read(self, path):
+                #self.n = int(sys.stdin.readline())
+                #self.parent = list(map(int, sys.stdin.readline().split()))
+                f = open(path, 'r').readlines()
+                self.n = int(f[0])
+                self.parent = list(map(int, f[1].split()))
+
 
         def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height)
-                return maxHeight
+                height = [0]*self.n
+
+                for node in range(0, self.n):
+                        n = node
+                        temp_queue = []
+                        temp = 0
+                        if self.parent[node] == -1:
+                                height[node] = 1
+                        elif height[node] != 0:
+                                continue
+                        else:
+                                while n != -1:
+                                        temp_queue.append(n)
+                                        n = self.parent[n]
+                                        if height[n] != 0:
+                                                temp = height[n]
+                                                break
+                                for c, i in enumerate(temp_queue):
+                                        height[i] = len(temp_queue) - c + temp
+                
+                return max(height)
+
 
 def main():
-  tree = TreeHeight()
-  tree.read()
-  print(tree.compute_height())
-
+        tree = TreeHeight()
+        for i in range(10, 24):
+                path = "/home/simon/Data_Structres_Coursework/Week 1/tree_height_tests/" + str(i)
+                tree.read(path)
+                t = '/home/simon/Data_Structres_Coursework/Week 1/tree_height_tests/' + str(i) + ".a"
+                f = open(t, 'r').readline()
+                print(tree.compute_height(), f)
 threading.Thread(target=main).start()
