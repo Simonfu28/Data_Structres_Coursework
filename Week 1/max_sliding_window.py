1d546f11454e98273ef44d20aa9a29ffd3883567
Simon <simonfu28@gmail.com>
2022-06-20 12:17:29 -0700

    Worked on assignment 5

--- Week 1/max_sliding_window.py
+++ Week 1/max_sliding_window.py
@@ -8,6 +8,15 @@ def max_sliding_window_naive(sequence, m):
 
     return maximums
 
+def max_sliding_window(sequence, m):
+    maximums = []
+    index = 0
+
+    for i in sequence:
+        if index >= m:
+            index = 0
+        
+
 if __name__ == '__main__':
     n = int(input())
     input_sequence = [int(i) for i in input().split()]
